e4ecd8b190449c0c44c44e400fcb75240334eb75
Phuntsok Drak-pa <phundrak@phundrak.fr>
2018-08-21 18:45:21 +0200

    Moved lots of files

--- clean.sh
+++ clean.sh
@@ -2,11 +2,12 @@
 
 if [ $# -eq 0 ]; then
     project_root=`pwd`
+    rm -f index.pdf
 else
     project_root=$1
 fi
 
-rm -rf *~ auto _minted* *.tex *.aux *.log *.toc *.out index.pdf headers.pdf headers.html lijokken.pdf old-hjapi.pdf zohaen.pdf
+rm -rf *~ auto _minted* *.tex *.aux *.log *.toc *.out headers.pdf headers.html
 for filename in *; do
     if [[ -d $filename ]]; then
         cd $filename
