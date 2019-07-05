271d5df2ce9410ff8bdd158f6c135664bd1250b5
Phuntsok Drak-pa <phundrak@phundrak.fr>
2019-07-05 21:49:56 +0200

    Reorganized repo
    Now the source files only will be on the main branch, whereas the html and pdf
    files will be on the gh-pages branch.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
-set delfname "index.pdf" "*.tex" "*.aux" "*.log" "*.toc" "*.out" "header.pdf" \
-             "header.html"
+set delfname "*.pdf" "*.tex" "*.aux" "*.log" "*.toc" "*.out" "*.html"
 set deldname "auto" "_minted*"
 for f in $delfname
     find -type f -name $f -delete
