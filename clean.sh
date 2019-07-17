35875ad0996194b8602a0888c70d4377b37630c0
Phuntsok Drak-pa <phundrak@phundrak.fr>
2019-07-17 14:49:52 +0200

    updated nyqy, added syntax, updated tree function

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set delfname "*.pdf" "*.tex" "*.aux" "*.log" "*.toc" "*.out" "*.html"
+set delfname "*.pdf" "*.tex" "*.aux" "*.log" "*.toc" "*.out" "*.html" "*.lol" "*.lot"
 set deldname "auto" "_minted*"
 for f in $delfname
     find -type f -name $f -delete
