d29cab7a1eaf93c248742e4a2a5bcf6d2b421f44
Lucien Cartier-Tilet <lucien@phundrak.com>
2020-05-05 14:49:57 +0200

    Separating this website’s backend from its content
    The backend has now its own repo, phundrak/org-website-backend

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sass --watch web/style/:web/style -tcompressed &
-webdev serve --release --hostname 0.0.0.0
--- web/dart/main.dart
+++
@@ -1,9 +0,0 @@
-import './reorganize_html.dart' show reorganizeHtml;
-import './theme.dart' show enableThemeChanger, setTheme;
-
-Future<void> main() async {
-  await setTheme();
-  await reorganizeHtml().then((_) {
-    enableThemeChanger();
-  });
-}
